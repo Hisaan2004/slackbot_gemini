8b3ba63a2cfef5b2573ffb9eff5570da89075ef4
hisaansakhawat <hisaan.sakhawat@gmail.com>
2025-07-31 17:23:44 +0500

    updating file

--- app/api/chatbot/route.js
+++ app/api/chatbot/route.js
@@ -175,7 +175,7 @@ export const handleUserQuestion = async (userPrompt,userId) => {
       if (lowerPrompt.includes("yes")) {
         meetingState.step = "name";
         await redis.set(`meetingState:${userId}`, meetingState);
-        return "Please enter your full name.";
+        return "Please enter your full name.if you wanted to discontinue the process enter No";
       } else if (lowerPrompt.includes("no")) {
       //  meetingState = { step: null, name: null, email: null, date: null, time: null, started: false };
         await redis.del(`meetingState:${userId}`);
@@ -185,24 +185,31 @@ export const handleUserQuestion = async (userPrompt,userId) => {
 
     // STEP 3: Capture each field sequentially
     if (meetingState.step === "name") {
+      if (userPrompt.includes("no")) {
+      //  meetingState = { step: null, name: null, email: null, date: null, time: null, started: false };
+      deletestate(userId);
+       // await redis.del(`meetingState:${userId}`);
+        return "Okay, no meeting will be scheduled.";
+      }
       meetingState.name = userPrompt;
       meetingState.step = "email";
       await redis.set(`meetingState:${userId}`, meetingState);
-      return "Please enter your email address.";
+      return "Please enter your email address.if you wanted to discontinue the process enter No";
     }
 
     if (meetingState.step === "email") {
       meetingState.email = userPrompt;
+
       meetingState.step = "date";
       await redis.set(`meetingState:${userId}`, meetingState);
-      return "Please enter the date for the meeting (format: DD-MM-YYYY).";
+      return "Please enter the date for the meeting (format: DD-MM-YYYY).if you wanted to discontinue the process enter No";
     }
 
     if (meetingState.step === "date") {
       meetingState.date = userPrompt;
       meetingState.step = "time";
       await redis.set(`meetingState:${userId}`, meetingState);
-      return "Please enter the time for the meeting between 11:00 and 17:00 (format: HH:MM).";
+      return "Please enter the time for the meeting between 11:00 and 17:00 (format: HH:MM).if you wanted to discontinue the process enter No";
     }
 
  /*   if (meetingState.step === "time") {
@@ -298,4 +305,6 @@ Let us know if you'd like to reschedule.`;
     return "Sorry, something went wrong while answering your question. Please try again later.";
   }
 };
-
+const deleteState = async (userId) => {
+  await redis.del(`meetingState:${userId}`);
+};
